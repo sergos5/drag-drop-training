7f9e693a96218219ab9d734f7948f562886485fa
Sergey Opekunov <sergos5@yandex.ru>
2024-12-08 10:41:02 +0300

    начало

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 'use strict'
 
-console.log('hi');
+const placeholders = document.querySelectorAll('.placeholder')
+const dragdropItem = document.querySelector('.item')
+
+const dragstart = (e)=> {
+    e.target.classList.add('hold')
+    setTimeout(()=> {
+        e.target.classList.add('hide')
+    },0)
+}
+
+const dragend = (e)=> {
+    e.target.classList.remove('hold', 'hide')
+}
+
+
+dragdropItem.addEventListener('dragstart', dragstart)
+
+dragdropItem.addEventListener('dragend', dragend)
